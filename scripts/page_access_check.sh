4904717f5f5f6c9a96a0e27f0578504659ed8ec1
Yuki <134689144+yuki10000@users.noreply.github.com>
2025-10-14 12:28:05 +0900

    #7 必要のないコメントを消した

--- scripts/page_access_check.sh
+++ scripts/page_access_check.sh
@@ -11,7 +11,7 @@ PAGE_ACCESSIBLE=false
 # Check page accessibility
 echo "Checking access to $URL ..."
 if curl --silent --head --fail "$URL" > /dev/null; then
-  echo "ページにアクセス可能です"
+  echo "Page is Accessible"
 
   # Attempt login if page is accessible
   echo "Attempting login..."
@@ -37,7 +37,7 @@ if curl --silent --head --fail "$URL" > /dev/null; then
     echo "CSRF token not found"
   fi
 else
-  echo "アクセス不可能です"
+  echo "Page is not accessible"
 fi
 
 # GitHub Actionsの$GITHUB_OUTPUTに書き込む
